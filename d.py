7ce400bb48357d5bb6d3a60c90ff2c20c0772215
JoyLeeA <dlwhdgk8@gmail.com>
2020-06-02 03:45:59 +0900

    2nd
    2nd

--- d.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import time
-count = 0
-pygame.mixer.init()
-music = pygame.mixer.Sound("/Users/joylee/Downloads/AI/warning.wav")
-if count == 0:
-    music.play()
-    time.sleep(5)
-   
